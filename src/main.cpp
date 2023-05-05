97238346d7c46fc2b8d968d46c7506f471f1cb78
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-05 19:48:35 +0200

    sgp30 added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com
-*********/
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "Adafruit_SGP30.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -15,6 +11,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 int Gas_analog = 4;  // used for ESP32
 int Gas_digital = 2; // used for ESP32
+
+Adafruit_SGP30 sgp;
 
 void setup()
 {
@@ -29,13 +27,28 @@ void setup()
   delay(2000);
 
   pinMode(Gas_digital, INPUT);
+
+  if (!sgp.begin())
+  {
+    Serial.println("Nie udało się uruchomić sensora SGP30. Sprawdź połączenie.");
+    while (1)
+      ;
+  }
 }
 
 void loop()
 {
-
   int gassensorAnalog = analogRead(Gas_analog);
   int gassensorDigital = digitalRead(Gas_digital);
+
+  if (!sgp.IAQmeasure())
+  {
+    Serial.println("Błąd podczas pomiaru IAQ!");
+    return;
+  }
+
+  int TVOC = sgp.TVOC;
+  int eCO2 = sgp.eCO2;
 
   // Clear the display
   display.clearDisplay();
@@ -56,6 +69,16 @@ void loop()
   display.println("A: ");
   display.setCursor(60, 10);
   display.println(gassensorAnalog);
+
+  // Display SGP30 measurements
+  display.setCursor(0, 30);
+  display.println("SGP30 TVOC: ");
+  display.setCursor(75, 30);
+  display.println(TVOC);
+  display.setCursor(0, 40);
+  display.println("SGP30 eCO2: ");
+  display.setCursor(75, 40);
+  display.println(eCO2);
 
   // Update the display
   display.display();
