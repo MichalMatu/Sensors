0282f4e92e5d4d60032067fb7cb724f1ca4c840f
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-05-22 00:59:12 +0200

    a

--- data/script.js
+++ data/script.js
@@ -2,17 +2,86 @@ function updateValues() {
   fetch("/values")
     .then((response) => response.text())
     .then((values) => {
-      // Update the content of the TVOC element
-      const tvocElement = document.getElementById("values");
-      tvocElement.innerHTML = values;
+      // Update the content of the values element
+      const valuesElement = document.getElementById("values");
+      valuesElement.innerHTML = values;
     })
     .catch((error) => {
       console.error("Error:", error);
     });
 }
 
-// Update the values initially
-updateValues();
+function fetchBuzzerStatus() {
+  fetch("/get-buzzer-status")
+    .then((response) => response.text())
+    .then((status) => {
+      updateBuzzerStatus(status);
+    })
+    .catch((error) => {
+      console.error("Error getting buzzer status:", error);
+    });
+}
 
-// Update the values every 1000ms
-setInterval(updateValues, 1000);
+function fetchRelayStatus() {
+  fetch("/get-relay-status")
+    .then((response) => response.text())
+    .then((status) => {
+      updateRelayStatus(status);
+    })
+    .catch((error) => {
+      console.error("Error getting relay status:", error);
+    });
+}
+
+function toggleBuzzer() {
+  fetch("/toggle-buzzer")
+    .then((response) => response.text())
+    .then((status) => {
+      updateBuzzerStatus(status);
+    })
+    .catch((error) => {
+      console.error("Error toggling buzzer:", error);
+    });
+}
+
+function toggleRelay() {
+  fetch("/toggle-relay")
+    .then((response) => response.text())
+    .then((status) => {
+      console.log("Relay state: " + status);
+    })
+    .catch((error) => {
+      console.error("Error toggling relay:", error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBuzzerStatus();
+  fetchRelayStatus();
+});
+
+function updateBuzzerStatus(status) {
+  const buzzerStatusElement = document.getElementById("buzzerStatus");
+  if (status === "on") {
+    buzzerStatusElement.textContent = "on";
+  } else {
+    buzzerStatusElement.textContent = "off";
+  }
+}
+
+function updateRelayStatus(status) {
+  const relayStatusElement = document.getElementById("relayStatus");
+  if (status === "on") {
+    relayStatusElement.textContent = "on";
+  } else {
+    relayStatusElement.textContent = "off";
+  }
+}
+
+function update() {
+  updateValues();
+  fetchBuzzerStatus();
+  fetchRelayStatus();
+}
+
+setInterval(update, 1000);
